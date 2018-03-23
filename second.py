f86df47dca1c081f632e0deea8d01771553b2519
santosje <santosje@grinnell.edu>
2018-03-23 17:45:45 -0500

    first commit

---
+++ second.py
@@ -0,0 +1,60 @@
+from keras import models
+from keras import layers
+from keras import optimizers
+from keras.datasets import imdb
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1.
+    return results
+
+
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=5,
+                    batch_size=512,
+                    validation_data=(x_val, y_val))
+
+history_dict = history.history
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+acc = history.history['acc']
+epochs = range(1, len(acc) + 1)
+
+
+plt.plot(epochs, loss_values, 'bo', label='Training loss')
+plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
